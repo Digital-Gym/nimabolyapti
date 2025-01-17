022018a2a28ff0e6ea1f7d613c047445c8c710dc
muminovbob <bobomurodm2004@gmail.com>
2025-01-17 21:38:54 +0500

    feat: pagination fixed

--- src/App.vue
+++ src/App.vue
@@ -7,28 +7,33 @@ import AppButton from "@/components/AppButton.vue";
 import { extractLink } from '@/utils';
 
 const data = ref();
-const nextUrl = ref();
+const firstUrl = ref();
 const prevUrl = ref();
+const nextUrl = ref();
+const lastUrl = ref();
 const userUrl = ref();
 const isLoading = ref(false);
 
-const handleNext = () => {
-  userUrl.value = nextUrl.value;
-  refresh();
-}
-
-const handlePrev = () => {
-  userUrl.value = prevUrl.value;
+const handleMove = (to) => {
+  switch(to){
+    case 'first': userUrl.value = firstUrl.value; break;
+    case 'prev': userUrl.value = prevUrl.value; break;
+    case 'next': userUrl.value = nextUrl.value; break;
+    case 'last': userUrl.value = lastUrl.value; break;
+  }
   refresh();
 }
 
 const refresh = async () => {
   isLoading.value = true;
   const response = await getSome(userUrl.value);
-  const link = response.link.split(" ");
+  const link = extractLink(response.link.split(" "));
 
-  nextUrl.value = extractLink(link[0]);
-  prevUrl.value = extractLink(link[2]);
+  firstUrl.value = link.first;
+  prevUrl.value = link.prev;
+  nextUrl.value = link.next;
+  lastUrl.value = link.last;
+  
   data.value = response.data.items;
   isLoading.value = false;
 }
@@ -44,8 +49,10 @@ onMounted(()=>{
       <h1 class="text-3xl">Repositories</h1>
       <!-- control panel -->
       <div class="flex gap-1">
-        <AppButton @click="handlePrev">Prev</AppButton>
-        <AppButton @click="handleNext">Next</AppButton>
+        <AppButton @click="handleMove('first')" :disabled="!firstUrl" color="gray">First</AppButton>
+        <AppButton @click="handleMove('prev')" :disabled="!prevUrl">Prev</AppButton>
+        <AppButton @click="handleMove('next')" :disabled="!nextUrl">Next</AppButton>
+        <AppButton @click="handleMove('last')" :disabled="!lastUrl" color="gray">Last</AppButton>
       </div>
       <!-- got data -->
       <div
@@ -61,10 +68,14 @@ onMounted(()=>{
           v-if="isLoading" 
           class="w-full h-full absolute bg-gray-50 opacity-35"
         />
+
         <div class="flex gap-1">
-          <AppButton @click="handlePrev">Prev</AppButton>
-          <AppButton @click="handleNext">Next</AppButton>
+          <AppButton @click="handleMove('first')" :disabled="!firstUrl">First</AppButton>
+          <AppButton @click="handleMove('prev')" :disabled="!prevUrl">Prev</AppButton>
+          <AppButton @click="handleMove('next')" :disabled="!nextUrl">Next</AppButton>
+          <AppButton @click="handleMove('last')" :disabled="!lastUrl">Last</AppButton>
         </div>
+
       </div>
       <AppLoader v-if="isLoading" />
     </div>
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,12 +1,42 @@
-export function extractLink(rawStr){
-  let result = "";
-  for(let i = 0; i < rawStr.length; i++){
-    if(rawStr[i] == '<'){
-      continue;
-    } else if(rawStr[i] == '>'){
+export function extractLink(arr){
+  return { 
+    first: extractCertain(arr, "first"),
+    next: extractCertain(arr, "next"),
+    prev: extractCertain(arr, "prev"),
+    last: extractCertain(arr, "last")
+  }  
+}
+
+function extractCertain(arr, keyword){
+  let index;
+
+  // find index of str containing keyword
+  for(let i = 0; i < arr.length; i++){
+    if(arr[i].split('"').some(x => x.trim() === keyword)){
+      index = i - 1;
       break;
     }
-    result += rawStr[i];
   }
+
+  if(index == undefined){
+    return null;
+  }
+
+  return sliceLink(arr[index]);
+}
+
+function sliceLink(str){
+  let result = "";
+
+  for(const i of str){
+    if(i == "<"){
+      continue;
+    }
+    if(i == ">"){
+      break;
+    }
+    result += i;
+  }
+
   return result;
 }
